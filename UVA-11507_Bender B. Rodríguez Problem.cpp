a2dbffae2e85119ba2519f4051d6ae55e64799ef
jahidhossain99 <54525656+jahidhossain99@users.noreply.github.com>
2020-08-16 18:36:32 +0600

    Create UVA-11507_Bender B. Rodríguez Problem.cpp
    It took me twelve submission ans one day to get it right.It was stressful but satisfying.

---
+++ UVA-11507_Bender B. Rodríguez Problem.cpp
@@ -0,0 +1,28 @@
+/*Emon Hossain
+ * Jahangirnagar University 
+ * 8/16/2020
+ */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    int n;cin>>n;
+    while(n){
+		string ans="+x";char ul='#';n--;
+		while(n--){
+			string s;cin>>s;
+		    if(ans[1]=='x'&&s[1]!='o'){
+                if(ans[0]=='+') ans=s;
+                else{
+                    ans[1]=s[1];ans[0]=(s[0]=='+'? '-':'+');}
+		    }
+		    else if(ans[1]==s[1]){
+		        if(s[0]==ans[0]) ans="-x";
+		        else ans="+x";
+		        }
+		}
+    cout<<ans<<"\n";
+	cin>>n;}
+}
+
